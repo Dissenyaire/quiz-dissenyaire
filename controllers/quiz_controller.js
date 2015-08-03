04d39cb9e8afe1f758861a1521f76de9977e29e9
Dissenyaire <Jose@MacBook-Pro-de-Jose.local>
2015-08-03 16:10:49 -0300

    Despliegue DB en Heroku

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,14 +2,14 @@ var models=require('../models/models.js');
 
 // GET /quizes/question
 exports.question = function(req, res) {
-  models.Quiz.findAll().success(function(quiz) {
+  models.Quiz.findAll().then(function(quiz) { // Cambiamos success por then por ser más actual
     res.render('quizes/question', { pregunta: quiz[0].pregunta});
     })
   };
 
 // GET /quizes/answer
 exports.answer = function(req, res) {
-    models.Quiz.findAll().success(function(quiz) {
+    models.Quiz.findAll().then(function(quiz) { // Cambiamos success por then por ser más actual
       if (req.query.respuesta === quiz[0].respuesta) {
         res.render('quizes/answer', { respuesta: 'Correcto' });
       } else {
