0d6ef98ea0b4218dbbd20544de333bb5e71a3667
Dissenyaire <Jose@MacBook-Pro-de-Jose.local>
2015-08-05 16:29:56 -0300

    Mudando index en routes

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,13 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controller');
 
-
 // Página de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz', errors: []});
-  });
+});
+
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
-
 
 // Definición de rutas de /quizes
 router.get('/quizes',                      quizController.index);
@@ -18,13 +17,5 @@ router.get('/quizes/:quizId(\\d+)',        quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
-
-// router.get('/author',function(req, res) {
-  //res.render('author', { title: 'Créditos' });
-
-  router.get('/quizes/author',function(req, res) {
-   res.render('/quizes/author', { title: 'Créditos' });
-  
-});
 
 module.exports = router;
