5fb991db537ed3ac43c7b84a11d1a1d7b9d8df69
Dissenyaire <Jose@MacBook-Pro-de-Jose.local>
2015-08-05 16:06:16 -0300

    Mudando path author

--- routes/index.js
+++ routes/index.js
@@ -19,8 +19,12 @@ router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
 router.get('/quizes/new',                  quizController.new);
 router.post('/quizes/create',              quizController.create);
 
-router.get('/author',function(req, res) {
-  res.render('author', { title: 'Créditos' });
+// router.get('/author',function(req, res) {
+  //res.render('author', { title: 'Créditos' });
+
+  router.get('/quizes/author',function(req, res) {
+   res.render('/quizes/author', { title: 'Créditos' });
+  
 });
 
 module.exports = router;
